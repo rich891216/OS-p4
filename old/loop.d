d94d7bad787bae666a08647afa96f22b695eae3b
Richard Wang <wangr@royal-28.cs.wisc.edu>
2021-03-18 12:57:43 -0500

    added system calls, need to define in proc.c

--- old/loop.d
+++
@@ -1,1 +0,0 @@
-loop.o: loop.c /usr/include/stdc-predef.h types.h user.h syscall.h
--- xv6/sysproc.c
+++ xv6/sysproc.c
@@ -89,3 +89,48 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// sets time slice for given proc
+int sys_setslice(void)
+{
+  int pid;
+  int slice;
+  if (argint(0, &pid) < 0 || argint(1, &slice)) {
+    return -1;
+  } else {
+    return setslice(pid, slice);
+  }
+}
+
+// gets time slice given pid
+int sys_getslice(void)
+{
+  int pid;
+  if (argint(0, &pid) < 0) {
+    return -1;
+  } else {
+    return getslice(pid);
+  }
+}
+
+//fork2, same as fork except can set time slice
+int sys_fork2(void)
+{
+  int slice;
+  if (argint(0, &slice) < 0) {
+    return -1;
+  } else {
+    return fork2(slice);
+  }
+}
+
+//gets pstat's information
+int sys_getpinfo(void)
+{
+  struct pstat *ps;
+  if (argptr(0, *ps, sizeof(struct pstat)) < 0) {
+    return -1;
+  } else {
+    return getpinfo(ps);
+  }
+}
