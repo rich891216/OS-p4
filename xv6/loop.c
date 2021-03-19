b4e7527be9f0f64b29743425433fea960b2e5aff
Richard Wang <wangr@rockhopper-05.cs.wisc.edu>
2021-03-18 20:44:16 -0500

    fixed loop.c, schedtest next

--- xv6/loop.c
+++ xv6/loop.c
@@ -7,26 +7,23 @@
 #include "syscall.h"
 #include "traps.h"
 #include "memlayout.h"
-
-void loop(int sleepticks) {
-    // sleep for <sleepticks> seconds
-    sleep(sleepticks);
-    int i = 0, j = 0;
-    while (i < 800000000) {
-        j += i * j + 1;
-        i++;
-    }
-}
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         // incorrect number of arguments
         exit();
     }
     int sleepticks = atoi(argv[1]);
-    if (sleepticks == 0) {
+    if (sleepticks <= 0) {
         // argv[1] can't be coverted
+        printf(2, "sleepticks is negative\n");
         exit();
     }
-    loop(sleepticks);
-    return 0;
+    sleep(sleepticks);
+    int i = 0;
+    int j = 0;
+    while (i < 800000000) {
+        j += i * j + 1;
+        i++;
+    }
+    exit();
 }
