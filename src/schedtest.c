250c140c6a1a6e53998b98a0bad795fe35c393f7
Richard Wang <wangr@royal-17.cs.wisc.edu>
2021-03-19 16:02:09 -0500

    idk why there are zombies

--- src/schedtest.c
+++ src/schedtest.c
@@ -5,26 +5,26 @@
 #include "pstat.h"
 #include "param.h"
 
-// static int schedtestFork(int slice, char *sleepT) {
-// 	int pid = fork2(slice);
-// 	int child = -1;
+static int schedtestFork(int slice, char *sleepT) {
+	int pid = fork2(slice);
+	int child = -1;
 
-// 	if (pid != -1) {
-// 		if (pid == 0) {
-// 			child = getpid();
-// 			char *args[] = {"loop", sleepT};
-// 			exec(args[0], args);
+	if (pid != -1) {
+		if (pid == 0) {
+			child = getpid();
+			char *args[] = {"loop", sleepT};
+			exec(args[0], args);
 
-// 			printf(2, "exec failed\n");
-// 			exit();
-// 		} else {
-// 			wait();
-// 		}
-// 	} else {
-// 		printf(2, "fork failed\n");
-// 	}
-// 	return child;
-// }
+			printf(2, "exec failed\n");
+			exit();
+		} else {
+			wait();
+		}
+	} else {
+		printf(2, "fork failed\n");
+	}
+	return child;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 6) {
@@ -39,56 +39,55 @@ int main(int argc, char *argv[]) {
 	struct pstat ps;
 
 	printf(1, "%d\n", sliceA);
-	// int childA = schedtestFork(sliceA, sleepA);
-	// int childB = schedtestFork(sliceB, sleepB);
+	int childA = schedtestFork(sliceA, sleepA);
+	int childB = schedtestFork(sliceB, sleepB);
 
-	int childA = fork2(sliceA);
-	int childB = fork2(sliceB);
-	if (childA == 0) {
-		char *args[] = {"loop", sleepA, 0};
-		exec(args[0], args);
-		exit();
-	} else {
-		if (childB == 0) {
-			char *args[] = {"loop", sleepB, 0};
-			exec(args[0], args);
-			exit();
-		} else {
-			sleep(sleepParent);
-			int compticksA = 0;
-			int compticksB = 0;
-			if (getpinfo(&ps) == 0) {
-				for(int i = 0; i < NPROC; i++) {
-					if (childA == ps.pid[i]) {
-						compticksA = ps.compticks[i];
-					} else if (childB == ps.pid[i]) {
-						compticksB = ps.compticks[i];
-					}
-				}
-				printf(1, "%d %d\n", compticksA, compticksB);
-				exit();
-			}
-		}
-	}
-	exit();
-	// sleep(sleepParent);
-
-	// int compticksA = 0;
-	// int compticksB = 0;
-
-	// if (getpinfo(&ps) == 0) {
-	// 	for (int i = 0; i < NPROC; i++) {
-	// 		if (childA == ps.pid[i]) {
-	// 			compticksA = ps.compticks[i];
-	// 		}
-
-	// 		if (childB == ps.compticks[i]) {
-	// 			compticksB = ps.compticks[i];
+	// int childA = fork2(sliceA);
+	// if (childA == 0) {
+	// 	char *args[] = {"loop", sleepA, 0};
+	// 	exec(args[0], args);
+	// 	kill(getpid());
+	// } else {
+	// 	int childB = fork2(sliceB);
+	// 	if (childB == 0) {
+	// 		char *args[] = {"loop", sleepB, 0};
+	// 		exec(args[0], args);
+	// 		kill(getpid());
+	// 	} else {
+	// 		sleep(sleepParent);
+	// 		int compticksA = 0;
+	// 		int compticksB = 0;
+	// 		if (getpinfo(&ps) == 0) {
+	// 			for(int i = 0; i < NPROC; i++) {
+	// 				if (childA == ps.pid[i]) {
+	// 					compticksA = ps.compticks[i];
+	// 				} else if (childB == ps.pid[i]) {
+	// 					compticksB = ps.compticks[i];
+	// 				}
+	// 			}
+	// 			printf(1, "%d %d\n", compticksA, compticksB);
+	// 			exit();
 	// 		}
 	// 	}
-	// 	printf(1, "%d %d\n", compticksA, compticksB);
-	// } else {
-	// 	printf(2, "Error: Could not access ptable correctly.\n");
 	// }
+	sleep(sleepParent);
+
+	int compticksA = 0;
+	int compticksB = 0;
+
+	if (getpinfo(&ps) == 0) {
+		for (int i = 0; i < NPROC; i++) {
+			if (childA == ps.pid[i]) {
+				compticksA = ps.compticks[i];
+			}
+
+			if (childB == ps.compticks[i]) {
+				compticksB = ps.compticks[i];
+			}
+		}
+		printf(1, "%d %d\n", compticksA, compticksB);
+	} else {
+		printf(2, "Error: Could not access ptable correctly.\n");
+	}
 	exit();
 }
